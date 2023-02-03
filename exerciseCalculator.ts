9c60a9aa527b12312ec063a63a6dcfa9d7992bff
TylerS2000 <tylerschulten@gmail.com>
2023-02-03 17:03:55 -0500

    linting and such

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,20 +1,20 @@
-function calculateExercises(trainingDays:Array<number>, dailyTargetHours:number){
+export default function calculateExercises(trainingDays:Array<number>, dailyTargetHours:number){
     const totalTrainingHours = trainingDays.reduce(
         (accumulator:number, currentValue:number)=>accumulator+currentValue,0
     );
 
-    const totalTargetHours:number = trainingDays.length*dailyTargetHours
+    const totalTargetHours:number = trainingDays.length*dailyTargetHours;
 
-    const daysTrained:number = trainingDays.reduce((accumulator:number,currentValue:number)=>{if(currentValue){accumulator+=1} return accumulator},0)
+    const daysTrained:number = trainingDays.reduce((accumulator:number,currentValue:number)=>{if(currentValue){accumulator+=1;} return accumulator;},0);
     
 
-    const success:boolean = totalTrainingHours>totalTargetHours
+    const success:boolean = totalTrainingHours>totalTargetHours;
 
-    const average:number =  totalTargetHours/trainingDays.length
+    const average:number =  totalTargetHours/trainingDays.length;
 
-    const rating = success?2:0
+    const rating = success?2:0;
 
-    const  ratingDescription = success?"good":"bad"
+    const  ratingDescription = success?"good":"bad";
 
     interface Result {
         periodLength:number,
@@ -33,11 +33,6 @@ function calculateExercises(trainingDays:Array<number>, dailyTargetHours:number)
         ratingDescription,
         target:totalTargetHours,
         average:average
-    }
-    console.log(Result)
+    };
+    console.log(Result);
 }
-const a: Array<number> = JSON.parse(process.argv[2])
-const b:number = Number(process.argv[3])
-
-
-calculateExercises(a,b)
--- hello.ts
+++ hello.ts
@@ -1,15 +1,26 @@
-import express from  'express'
+import express from  'express';
+import calculateBmi from './calculateBmi';
+//import qs from 'qs'
 
-
-const app = express()
+const app = express();
 
 app.get('/hello',(_req,res)=>{
-    res.send("hello")
-})
+    res.send("hello");
+});
+
+app.get('/bmi',(req,res)=>{
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+    if(!height||!weight){
+        res.send("bad params");
+    }
+    else{
+    const result = calculateBmi(height,weight);
+    res.send(result);}
+});
 
 app.listen(3000,()=>{
     console.log("running");
-    
-})
+});
 
 
